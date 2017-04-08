f5f6f53862ba1fe52fe7ddf2ebea4fcffa89d1ee
jsxii <jsxii@users.noreply.github.com>
2017-04-08 13:30:21 +0300

    12th

--- _push.cmd
+++ _push.cmd
@@ -1,7 +1,11 @@
 @echo off
 cd C:\Local\_github\mc.ATm.8.Common\
+echo -- add --
 c:\usr\git\bin\git.exe add .
+echo -- stage --
 c:\usr\git\bin\git.exe stage
+echo -- commit --
 c:\usr\git\bin\git.exe commit -m "12th"
+echo -- push --
 c:\usr\git\bin\git.exe push 
 

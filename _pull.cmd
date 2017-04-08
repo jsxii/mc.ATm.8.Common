4f55fe31738d145db975c0c398b374708bc17bf7
jsxii <jsxii@users.noreply.github.com>
2017-04-08 13:29:02 +0300

    12th

---
+++ _pull.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Local\_github\mc.ATm.8.Common\
+c:\usr\git\bin\git.exe pull origin master
---
+++ _push.cmd
@@ -0,0 +1,7 @@
+@echo off
+cd C:\Local\_github\mc.ATm.8.Common\
+c:\usr\git\bin\git.exe add .
+c:\usr\git\bin\git.exe stage
+c:\usr\git\bin\git.exe commit -m "12th"
+c:\usr\git\bin\git.exe push 
+

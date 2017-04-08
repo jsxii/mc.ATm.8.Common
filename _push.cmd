39c32883f7059410e45ba320899831c8f49616e2
jsxii <jsxii@users.noreply.github.com>
2017-04-08 13:42:45 +0300

    12th

--- _push.cmd
+++ _push.cmd
@@ -1,9 +1,9 @@
 @echo off
 cd C:\Local\_github\mc.ATm.8.Common\
 echo -- rm --
-c:\usr\git\bin\git.exe rm -r --cashed .\
+c:\usr\git\bin\git.exe rm -r --cashed .
 echo -- add --
-c:\usr\git\bin\git.exe add --all .\
+c:\usr\git\bin\git.exe add --all .
 echo -- stage --
 c:\usr\git\bin\git.exe stage
 echo -- commit --

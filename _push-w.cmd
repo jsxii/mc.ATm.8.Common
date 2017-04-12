ce93057e3162f123246a80df2686f67133e54444
Малинин Иван Викторович <ivmalin1@mts.ru>
2017-04-12 17:26:16 +0300

    0300wsbss00182\ivm

--- _push-w.cmd
+++ _push-w.cmd
@@ -7,7 +7,7 @@ c:\usr\git\bin\git.exe add --all .\
 echo -- stage --
 c:\usr\git\bin\git.exe stage
 echo -- commit --
-c:\usr\git\bin\git.exe commit -m "game\12th"
+c:\usr\git\bin\git.exe commit -m "0300wsbss00182\ivm"
 echo -- push --
 c:\usr\git\bin\git.exe push --set-upstream origin master
 

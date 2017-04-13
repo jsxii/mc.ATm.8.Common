e4d0348c812d031299ab722b505a71699f2a32b6
jsxii <jsxii@users.noreply.github.com>
2017-04-13 22:16:36 +0300

    TELCO-10\root

--- _push-d-usr.cmd
+++ _push-d-usr.cmd
@@ -6,7 +6,7 @@ d:\usr\git\bin\git.exe add --all .\
 echo -- stage --
 d:\usr\git\bin\git.exe stage
 echo -- commit --
-d:\usr\git\bin\git.exe commit -m "game\root"
+d:\usr\git\bin\git.exe commit -m "%computername%\%username%"
 echo -- push --
 d:\usr\git\bin\git.exe push --set-upstream origin master
 
